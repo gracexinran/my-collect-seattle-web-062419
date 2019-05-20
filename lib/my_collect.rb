8480dd4675f61ce3ddb88648fff9407e00ca6c84
Xinran Che <gracexinran@gmail.com>
2019-05-20 05:49:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,3 +6,11 @@ def my_collect(collection)
   end
   collection
 end
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
